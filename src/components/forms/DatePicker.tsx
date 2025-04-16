3cd9a2acce3e0af1386988428adc2312be93c9dc
C4ncino <193305@iberopuebla.mx>
2025-04-15 23:12:15 -0600

    feat: add datetime picker

---
+++ src/components/forms/DatePicker.tsx
@@ -0,0 +1,60 @@
+import colors from "tailwindcss/colors";
+import { useColorScheme } from "nativewind";
+import { Calendar } from "iconoir-react-native";
+import { View, Text, Pressable } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+import Label from "./Label";
+
+import useDate from "@/hooks/useDate";
+import useModal from "@/hooks/useModal";
+import { lang } from "@/utils/formatters";
+
+interface Props {
+  label?: string;
+  value: number;
+  onSelect: (value: number) => void;
+}
+
+const DatePicker = ({ label, value, onSelect }: Props) => {
+  const { colorScheme } = useColorScheme();
+  const isDark = colorScheme === "dark";
+
+  const { dateObject, dateLong } = useDate(value);
+  const { openModal, closeModal, visible } = useModal();
+
+  const handleConfirm = (date: Date) => {
+    onSelect(date.getTime());
+    closeModal();
+  };
+
+  return (
+    <>
+      <View>
+        <Label label={label} />
+        <Pressable
+          className="h-12 px-2 flex-row justify-between items-center border rounded-md"
+          onPress={openModal}
+        >
+          <Text className="dark:text-white">{dateLong}</Text>
+
+          <Calendar
+            width={24}
+            height={24}
+            color={isDark ? colors.white : colors.zinc[900]}
+          />
+        </Pressable>
+      </View>
+      <DateTimePickerModal
+        locale={lang}
+        date={dateObject}
+        isVisible={visible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={closeModal}
+      />
+    </>
+  );
+};
+
+export default DatePicker;
--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,12 +1,17 @@
 import { useMemo } from "react";
 
+import { lang } from "@/utils/formatters";
+
 const useDate = (timestamp: number) => {
   const dateObject = useMemo(() => new Date(timestamp), [timestamp]);
-  const date = dateObject.toLocaleDateString();
+  const dateShort = dateObject.toLocaleDateString();
+  const dateLong = dateObject.toLocaleDateString(lang, { year: "numeric", weekday: "long", month: "long", day: "numeric" });
   const time = dateObject.toLocaleTimeString();
 
   return {
-    date,
+    dateObject,
+    dateShort,
+    dateLong,
     time,
   };
 };
